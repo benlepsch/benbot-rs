262627d07e5c9b89a8c704d0041b65986245f669
Ben Lepsch <blepsch@gmail.com>
2025-04-11 01:58:30 +0000

    separate commands into new file

---
+++ src/commands.rs
@@ -0,0 +1,73 @@
+use crate::{Context, Error};
+use poise::serenity_prelude as serenity;
+
+use rand;
+use rand::seq::IndexedRandom;
+
+pub struct Data {}
+
+/* Commands */
+
+// says hello
+#[poise::command(slash_command, prefix_command)]
+pub async fn say_hello(
+	ctx: Context<'_>
+) -> Result<(), Error> {
+	ctx.say("i am poising").await?;
+	Ok(())
+}
+
+#[poise::command(slash_command, prefix_command)]
+pub async fn say(
+	ctx: Context<'_>,
+	#[description = "what to say"] text: String,
+	#[description = "who is saying"] user: serenity::User,
+) -> Result<(), Error> {
+	let saying = format!("{} says: {}", user.name, text);
+	
+	ctx.say(saying).await?;
+	Ok(())
+}
+
+// register command
+#[poise::command(prefix_command)]
+pub async fn register(ctx: Context<'_>) -> Result<(), Error> {
+	poise::builtins::register_application_commands_buttons(ctx).await?;
+	Ok(())
+}
+
+
+const PAPRIKA_GIFS: &'static [&str] = &[
+    "https://tenor.com/view/paprika-movie-anime-atsuko-chiba-satoshi-kon-gif-14134517",
+    "https://tenor.com/view/paprika-gif-5430367",
+    "https://tenor.com/view/paprika-gif-25394130",
+    "https://tenor.com/view/paprika-paprika-anime-gif-10413276",
+    "https://media.tenor.com/wxaaQuEOXQAAAAAC/anime-burger.gif",
+];
+
+// message handler for paprika
+pub async fn event_handler(
+    ctx: &serenity::Context,
+    event: &serenity::FullEvent,
+    _framework: poise::FrameworkContext<'_, Data, Error>,
+    _data: &Data,
+) -> Result<(), Error> {
+    match event {
+        serenity::FullEvent::Ready { data_about_bot, .. } => {
+            println!("Logged in as {}", data_about_bot.user.name);
+        }
+        serenity::FullEvent::Message { new_message } => {
+            if new_message.content.to_lowercase().contains("paprika")
+                && new_message.author.id != ctx.cache.current_user().id {
+                let chosen_gif = {
+                    let mut rng = rand::rng();
+                    PAPRIKA_GIFS.choose(&mut rng).expect("should pick a gif")
+                };
+
+                new_message.reply(ctx, *chosen_gif).await?;
+            }
+        }
+        _ => {}
+    }
+    Ok(())
+}
--- src/main.rs
+++ src/main.rs
@@ -1,48 +1,14 @@
+mod commands;
+
 use poise::serenity_prelude as serenity;
 use std::env;
 
-use rand;
-use rand::seq::IndexedRandom;
-struct Data {}
-
 type Error = Box<dyn std::error::Error + Send + Sync>;
-type Context<'a> = poise::Context<'a, Data, Error>;
-
-
-/* Commands */
-
-// says hello
-#[poise::command(slash_command, prefix_command)]
-async fn say_hello(
-	ctx: Context<'_>
-) -> Result<(), Error> {
-	ctx.say("i am poising").await?;
-	Ok(())
-}
-
-#[poise::command(slash_command, prefix_command)]
-async fn say(
-	ctx: Context<'_>,
-	#[description = "what to say"] text: String,
-	#[description = "who is saying"] user: serenity::User,
-) -> Result<(), Error> {
-	let saying = format!("{} says: {}", user.name, text);
-	
-	ctx.say(saying).await?;
-	Ok(())
-}
-
-// register command
-#[poise::command(prefix_command)]
-async fn register(ctx: Context<'_>) -> Result<(), Error> {
-	poise::builtins::register_application_commands_buttons(ctx).await?;
-	Ok(())
-}
-
+type Context<'a> = poise::Context<'a, commands::Data, Error>;
 
 /* Error handling */
 
-async fn on_error(error: poise::FrameworkError<'_, Data, Error>) {
+async fn on_error(error: poise::FrameworkError<'_, commands::Data, Error>) {
 	match error {
 		poise::FrameworkError::Setup { error, .. } => panic!("Failed to start bot: {:?}", error),
 		poise::FrameworkError::Command {error, ctx, ..} => {
@@ -57,14 +23,6 @@ async fn on_error(error: poise::FrameworkError<'_, Data, Error>) {
 	}
 }
 
-const PAPRIKA_GIFS: &'static [&str] = &[
-    "https://tenor.com/view/paprika-movie-anime-atsuko-chiba-satoshi-kon-gif-14134517",
-    "https://tenor.com/view/paprika-gif-5430367",
-    "https://tenor.com/view/paprika-gif-25394130",
-    "https://tenor.com/view/paprika-paprika-anime-gif-10413276",
-    "https://media.tenor.com/wxaaQuEOXQAAAAAC/anime-burger.gif",
-];
-
 #[tokio::main]
 async fn main() {
     let token = env::var("BENBOT_TOKEN")
@@ -75,13 +33,13 @@ async fn main() {
     let framework = poise::Framework::builder()
         .setup(move |_ctx, _ready, _framework| {
             Box::pin(async move {
-                Ok(Data { })
+                Ok(commands::Data { })
             })
         })
         .options(poise::FrameworkOptions {
-			commands: vec![say_hello(), say(), register()],
-            event_handler: |ctx, event, framework, data| {
-                Box::pin(event_handler(ctx, event, framework, data))
+			commands: vec![commands::say_hello(), commands::say(), commands::register()],
+            event_handler: |ctx, event, framework, commands::Data {}| {
+                Box::pin(commands::event_handler(ctx, event, framework, &commands::Data {}))
             },
             on_error: |error| Box::pin(on_error(error)),
             ..Default::default()
@@ -93,30 +51,4 @@ async fn main() {
         .await;
 
     client.unwrap().start().await.unwrap();
-}
-
-async fn event_handler(
-    ctx: &serenity::Context,
-    event: &serenity::FullEvent,
-    _framework: poise::FrameworkContext<'_, Data, Error>,
-    _data: &Data,
-) -> Result<(), Error> {
-    match event {
-        serenity::FullEvent::Ready { data_about_bot, .. } => {
-            println!("Logged in as {}", data_about_bot.user.name);
-        }
-        serenity::FullEvent::Message { new_message } => {
-            if new_message.content.to_lowercase().contains("paprika")
-                && new_message.author.id != ctx.cache.current_user().id {
-                let chosen_gif = {
-                    let mut rng = rand::rng();
-                    PAPRIKA_GIFS.choose(&mut rng).expect("should pick a gif")
-                };
-
-                new_message.reply(ctx, *chosen_gif).await?;
-            }
-        }
-        _ => {}
-    }
-    Ok(())
 }
